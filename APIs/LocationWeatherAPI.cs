0cf43520bc3d67afd4d413f61943864519c404d6
Harriethw <harriethw@gmail.com>
2017-09-01 16:50:39 -0400

    Add files via upload

---
+++ APIs/LocationWeatherAPI.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+public class LocationWeatherAPI : MonoBehaviour {
+	//public string url = "http://api.openweathermap.org/data/2.5/weather?lat=43.667027&lon=-79.424759&APPID=391013b34d8368540317a2dd0dd2536d";
+	public string city;
+	public string weatherDescription;
+	public float temp;
+	public float temp_min;
+	public float temp_max;
+	public float rain;
+	public float wind;
+	public float clouds;
+
+	public string currentIP;
+	public string currentCountry;
+	public string currentCity;
+	public string APPID = "&APPID=391013b34d8368540317a2dd0dd2536d";
+
+	private SkyAPI sky;
+	private ChimeController chimes;
+
+	void Awake (){
+
+		sky = WeatherAPI.FindObjectOfType<SkyAPI>();
+		chimes = WeatherAPI.FindObjectOfType<ChimeController>();
+	}
+
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+		//get the players IP, City, Country
+		//Network.Connect("https://api.ipify.org");
+		//currentIP = Network.player.externalIP;
+		//Network.Disconnect();
+
+		currentIP = new System.Net.WebClient().DownloadString("https://api.ipify.org");
+
+		WWW cityRequest = new WWW("http://freegeoip.net/json/" + currentIP); //get our location info
+		yield return cityRequest;
+
+		if (cityRequest.error == null || cityRequest.error == "")
+		{
+			var N = JSON.Parse(cityRequest.text);
+			currentCity = N["city"].Value;
+			currentCountry = N["country_name"].Value;
+		}
+
+		else
+		{
+			Debug.Log("WWW error: " + cityRequest.error);
+		}
+		//go to the api
+		WWW request = new WWW("http://api.openweathermap.org/data/2.5/weather?q=" + currentCity + APPID);
+		yield return request;
+		if (request.error == null || request.error == "") 
+		{
+			setWeatherAttributes(request.text);
+		} 
+		else 
+		{
+			Debug.Log("Error: " + request.error);
+		}
+
+	}
+
+	void setWeatherAttributes(string jsonString) {
+		var weatherJson = JSON.Parse(jsonString);
+		city = weatherJson["name"].Value;
+		weatherDescription = weatherJson["weather"][0]["description"].Value;
+		temp = weatherJson["main"]["temp"].AsFloat;
+		temp_min = weatherJson["main"]["temp_min"].AsFloat;
+		temp_max = weatherJson["main"]["temp_max"].AsFloat;
+		rain = weatherJson["rain"]["3h"].AsFloat;
+		clouds = weatherJson["clouds"]["all"].AsInt;
+		wind = weatherJson["wind"]["speed"].AsFloat;
+
+		sky.SkyChoice ();
+		chimes.ChimeChoice();
+	}
+
+}
---
+++ Wind/WindComponent.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindComponent : MonoBehaviour {
+
+		// To use:
+		// 1. Create an empty game object
+		// Add a Collider2D to the empty object. I used a standard Box Collider 2D, as a trigger, to create an isolated square that applies "wind"
+		// 3. Add this WindComponent to the empty object, then adjust the Force (Vector2). You can adjust this in the editor or in the script, as it is a public property
+		// Note: Only works on game objects that have the Rigid Body 2D and Collider 2D components
+
+		// Directional force applied to objects that enter this object's Collider 2D boundaries
+		public Vector3 Force = Vector3.zero;
+
+		// Internal list that tracks objects that enter this object's "zone"
+		private List<Collider> objects = new List<Collider>();
+
+		private WeatherAPI weather;
+	private float windspeed;
+
+		void Start (){
+		weather = WeatherAPI.FindObjectOfType<WeatherAPI>();
+		windspeed = weather.wind;
+		Force.x = windspeed;
+		Debug.Log (windspeed);
+		}
+		void FixedUpdate()
+		{
+
+		windspeed = weather.wind;
+		Force.x = windspeed;
+
+			// For every object being tracked
+			for(int i = 0; i < objects.Count; i++)
+			{
+
+
+
+				// Get the rigid body for the object.
+				Rigidbody body = objects[i].attachedRigidbody;
+
+				// Apply the force
+				body.AddForce(Force);
+			}
+		}
+
+	void OnTriggerEnter(Collider other)
+		{
+			objects.Add(other);
+		}
+
+		void OnTriggerExit(Collider other)
+		{
+			objects.Remove(other);
+		}
+	}
+
